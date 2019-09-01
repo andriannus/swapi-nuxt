f7fc1053516afacd3b88d43ac690332346420d90
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2019-09-01 23:00:53 +0700

    add AppBanner component

---
+++ components/AppBanner.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="bra-5 has-text-centered hero is-family-secondary is-dark">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">{{ title }}</h1>
+        <h2 class="subtitle">{{ subtitle }}</h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      default: '',
+      type: String
+    },
+    subtitle: {
+      default: '',
+      type: String
+    }
+  }
+}
+</script>
