f787285d01d80b52992ef349fb01c1959068be90
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2019-09-01 23:00:40 +0700

    add const and util

--- assets/const/swapi.js
+++ assets/const/swapi.js
@@ -1,3 +1,40 @@
-export const SWAPI = {
-  CATEGORIES: ['films', 'people', 'planets', 'species', 'starships', 'vehicles']
-}
+export const CATEGORIES = [
+  'films',
+  'people',
+  'planets',
+  'species',
+  'starships',
+  'vehicles'
+]
+
+export const RESOURCES = [
+  {
+    title: 'Films',
+    subtitle: 'A Film resource is a single film'
+  },
+  {
+    title: 'People',
+    subtitle:
+      'A People resource is an individual person or character within the Star Wars universe'
+  },
+  {
+    title: 'Planets',
+    subtitle:
+      'A Planet resource is a large mass, planet or planetoid in the Star Wars Universe, at the time of 0 ABY'
+  },
+  {
+    title: 'Species',
+    subtitle:
+      'A Species resource is a type of person or character within the Star Wars Universe'
+  },
+  {
+    title: 'Starships',
+    subtitle:
+      'A Starship resource is a single transport craft that has hyperdrive capability'
+  },
+  {
+    title: 'Vehicles',
+    subtitle:
+      'A Vehicle resource is a single transport craft that does not have hyperdrive capability'
+  }
+]
---
+++ utils/transform.js
@@ -0,0 +1,4 @@
+export function capitalize(value) {
+  if (typeof value !== 'string') return ''
+  return value.charAt(0).toUpperCase() + value.slice(1)
+}
